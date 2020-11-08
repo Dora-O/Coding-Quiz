71b17c94c702bb6bd7662bc2765deff313d11ede
Dora O <dorozco2390@utexas.edu>
2020-11-07 18:30:15 -0600

    re added functions that were removed earlier

--- script.js
+++ script.js
@@ -45,6 +45,30 @@ var questions = [{
 },
 ];
 
+function start(){
+    timeLeft = 90;
+    document.getElementById("timeLeft")=timeLeft;
 
+    timer = setInterval(function(){
+        timeLeft--;
+        document.getElementById("timeLeft")=timeLeft;
 
+        if (timeLeft<= 0){
+            clearInterval(timer);
+            endGame();
+        }
+    })
+}
+
+function endGame(){
+    clearInterval(timer);
+    var quizEnding = `
+    <h2>Game over!</h2>
+    <h3>You got a ` + score +  ` /100!</h3>
+    <h3>That means you got ` + score / 20 +  ` questions correct!</h3>
+    <input type="text" id="name" placeholder="First name"> 
+    <button onclick="setScore()">Set score!</button>`;
+
+    document.getElementById("quizQuestions")= quizEnding;
+}
 
