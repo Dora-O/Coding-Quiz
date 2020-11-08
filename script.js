5d396d0217eecfcbbc1e18605a4617dc62c7f108
Dora O <dorozco2390@utexas.edu>
2020-11-07 18:40:50 -0600

    added setscore and getscore function

--- script.js
+++ script.js
@@ -94,3 +94,21 @@ function nextQuestion() {
 
     document.getElementById("quizQuestions").innerHTML = quizContent;
 }
+
+function setScore() {
+    localStorage.setItem("highscore", score);
+    localStorage.setItem("highscoreName", document.getElementById('name').value);
+    getScore();
+}
+
+function getScore() {
+    var quizContent = `
+    <h2>` + localStorage.getItem("highscoreName") + `'s highscore is:</h2>
+    <h1>` + localStorage.getItem("highscore") + `</h1><br> 
+    
+    <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>
+    
+    `;
+
+    document.getElementById("quizQuestions") = quizContent;
+}
